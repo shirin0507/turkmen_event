85c6ba158d747280ce6263622baa59a52def02be
shirin0507 <166390137+shirin0507@users.noreply.github.com>
2024-07-19 16:49:47 +0500

    1.3

--- database/migrations/2024_07_19_113034_create_venues_table.php
+++ database/migrations/2024_07_19_113034_create_venues_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('venues', function (Blueprint $table) {
             $table->id();
+        $table->string('name');
+        $table->string('address');
+        $table->string('city');
+        $table->string('country');
             $table->timestamps();
         });
     }
