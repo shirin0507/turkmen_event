578779412c72032f9a7e1cc15ed4fa9aea906a1e
shirin0507 <166390137+shirin0507@users.noreply.github.com>
2024-07-19 17:41:07 +0500

    2.1

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -17,7 +17,10 @@ class TicketFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'type' => fake()->randomElement(['General Admission', 'VIP', 'Early Bird']),
+            'price' => fake()->randomFloat(2, 10, 200),
+            'quantity' => fake()->numberBetween(50, 200),
+            'event_id' => factory(App\Models\Event::class),
         ];
     }
 }
