1d12547b4ec0d1a1139f4cd3339cebefe858fcb8
shirin0507 <166390137+shirin0507@users.noreply.github.com>
2024-07-19 16:59:12 +0500

    1.5

--- database/migrations/2024_07_19_113113_create_orders_table.php
+++ database/migrations/2024_07_19_113113_create_orders_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->foreignId('ticket_id')->constrained()->onDelete('cascade');
+        $table->integer('quantity');
+        $table->decimal('total_price', 8, 2);
             $table->timestamps();
         });
     }
