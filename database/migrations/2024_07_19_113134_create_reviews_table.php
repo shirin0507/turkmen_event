2b1072f3685fea32ec14aa0881ee77f7cfbae350
shirin0507 <166390137+shirin0507@users.noreply.github.com>
2024-07-19 17:04:09 +0500

    1.6

--- database/migrations/2024_07_19_113134_create_reviews_table.php
+++ database/migrations/2024_07_19_113134_create_reviews_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+        $table->foreignId('event_id')->constrained()->onDelete('cascade');
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->text('review');
+        $table->integer('rating');
             $table->timestamps();
         });
     }
