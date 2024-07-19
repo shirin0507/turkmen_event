02ee947805587fd6a6b0cd3e00fa112a42a01b07
shirin0507 <166390137+shirin0507@users.noreply.github.com>
2024-07-19 17:30:35 +0500

    1.9

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -17,7 +17,13 @@ class EventFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(),
+            'description' => fake()->paragraph(),
+            'start_time' => fake()->dateTime(),
+            'end_time' => fake()->dateTime(),
+            'venue_id' => App\Models\Venue::inRandomOrder()->first()->id,
+            'category_id' => App\Models\Category::inRandomOrder()->first()->id,
+            'organizer_id' => App\Models\Organizer::inRandomOrder()->first()->id,
         ];
     }
 }
