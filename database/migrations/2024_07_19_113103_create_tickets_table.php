c1f07007fa79c9ca54dccbe59f2ac55bb3b74e11
shirin0507 <166390137+shirin0507@users.noreply.github.com>
2024-07-19 16:55:25 +0500

    1.4

--- database/migrations/2024_07_19_113103_create_tickets_table.php
+++ database/migrations/2024_07_19_113103_create_tickets_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+        $table->string('type');
+        $table->decimal('price', 8, 2);
+        $table->integer('quantity');
+        $table->foreignId('event_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
